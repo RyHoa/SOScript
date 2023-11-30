51755f25a22f4a396e80f011c882a96452b227a1
Ryan Hoang <ryanbh1991@gmail.com>
2023-11-29 22:55:24 -0500

    Added Echo commands, Added Elasticsearch Script

---
+++ EGupdateElasticsearch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd /home/admin/securityonion
+# update to latest
+sudo git pull origin 2.4/dev
+echo Pulled latest edition.
+# regen source packages
+cd /home/admin/securityonion/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -rf source-packages
+sudo pip download -r requirements.txt -d source-packages
+# remove old ES files
+sudo rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo rm -f /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+echo Removed old files...
+# copy over new files
+cd ..
+sudo cp -r elasticsearch /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
+sudo cp -r elasticsearch /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+cd /home/admin/securityonion/salt/sensoroni/
+sudo cp defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+sudo cp soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+echo Copied new files.
+sudo so-sensoroni-restart --force
+echo Complete.
--- RHscript.sh
+++ RHscript.sh
@@ -1,55 +1,23 @@
 #!/bin/bash
-#gets to right directory and git pull code
 cd /home/admin/securityonion
-git checkout 2.4/dev
-git pull origin 2.4/dev
-
-#remove source-packages
-rm -rf /home/admin/securityonion/salt/sensoroni/files/analyzers/threatfox/source-packages
-rm -rf /home/admin/securityonion/salt/sensoroni/files/analyzers/malwarebazaar/source-packages
-rm -rf /home/admin/securityonion/salt/sensoroni/files/analyzers/echotrail/source-packages
+git pull
 rm -rf /home/admin/securityonion/salt/sensoroni/files/analyzers/elasticsearch/source-packages
-
-#install source-packages
-cd /home/admin/securityonion/salt/sensoroni/files/analyzers/threatfox
+cd salt/sensoroni/files/analyzers/elasticsearch
 pip download -r requirements.txt -d source-packages
-cd /home/admin/securityonion/salt/sensoroni/files/analyzers/malwarebazaar
-pip download -r requirements.txt -d source-packages
-cd /home/admin/securityonion/salt/sensoroni/files/analyzers/echotrail
-pip download -r requirements.txt -d source-packages
-cd /home/admin/securityonion/salt/sensoroni/files/analyzers/elasticsearch
-pip download -r requirements.txt -d source-packages
-
-#remove defaults and soc_sensoroni yaml
-rm -rf /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
-rm -rf /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
-
-cd /home/admin/securityonion/salt/sensoroni/
-cp -r defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
-cp -r soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
-
-#remove the analyzers from default and local
-rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/threatfox
-rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/threatfox
-rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/malwarebazaar
-rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/malwarebazaar
-rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/echotrail
-rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/echotrail
+#remove the elasticsearch from default and local
 rm -rf /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
 rm -rf /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
-
-#copies the analzyers to the to default and local
-cd /home/admin/securityonion/salt/sensoroni/files/analyzers
-cp -r threatfox /opt/so/saltstack/default/salt/sensoroni/files/analyzers/threatfox
-cp -r threatfox /opt/so/saltstack/local/salt/sensoroni/files/analyzers/threatfox
-cp -r malwarebazaar /opt/so/saltstack/default/salt/sensoroni/files/analyzers/malwarebazaar
-cp -r malwarebazaar /opt/so/saltstack/local/salt/sensoroni/files/analyzers/malwarebazaar
-cp -r echotrail /opt/so/saltstack/default/salt/sensoroni/files/analyzers/echotrail
-cp -r echotrail /opt/so/saltstack/local/salt/sensoroni/files/analyzers/echotrail
+cd ..
 cp -r elasticsearch /opt/so/saltstack/default/salt/sensoroni/files/analyzers/elasticsearch
 cp -r elasticsearch /opt/so/saltstack/local/salt/sensoroni/files/analyzers/elasticsearch
+rm -rf /opt/so/saltstack/local/salt/sensoroni/defaults.yaml
+rm -rf /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+rm -rf /opt/so/saltstack/local/salt/sensoroni/soc_sensoroni.yaml
+rm -rf /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+cd /home/admin/securityonion/salt/sensoroni/
+cp -r defaults.yaml /opt/so/saltstack/default/salt/sensoroni/defaults.yaml
+cp -r defaults.yaml /opt/so/saltstack/local/salt/sensoroni/defaults.yaml
+cp -r soc_sensoroni.yaml /opt/so/saltstack/default/salt/sensoroni/soc_sensoroni.yaml
+cp -r soc_sensoroni.yaml /opt/so/saltstack/local/salt/sensoroni/soc_sensoroni.yaml
 
 so-sensoroni-restart
-
-
-
